1c8d9068e8186e95d143e9abf06443cb5f835e40
SunilYdv01 <95336927+SunilYdv01@users.noreply.github.com>
2024-07-24 08:07:52 +0000

    Vaiables

---
+++ basics/variables.js
@@ -0,0 +1,9 @@
+const accoundId = 12345
+var accountEmail ="abc@google.com"
+let accountPass = "1234"
+accountCity = "Noida"
+console.log(accoundId);
+
+console.log(accountEmail);
+console.log(accountPass);
+console.table([accoundId,accountEmail,accountPass,accountCity])
